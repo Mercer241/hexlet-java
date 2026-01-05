8dddd42b12d756a9d5e4f0bafac957b883e91f1f
Александр Приходько <sasha-1337@mail.ru>
2026-01-05 23:52:49 +0500

    Use Apache Commons Lang to reverse string

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,10 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
-public static void main(String[] args) {
-Two.greeting();
-}
+    public static void main(String[] args) {
+        String reversed = StringUtils.reverse("Hexlet");
+        System.out.println(reversed);
+    }
 }
